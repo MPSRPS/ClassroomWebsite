6710f58a6fe9967eed2d5545d331d334f8315653
Manohar Sonsale <86419036+MPSRPS@users.noreply.github.com>
2024-08-13 22:16:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv")
 // const bodyParser = require("body-parser")
 const app = express()
 const Routes = require("./routes/route.js")
-require("dotenv").config
+
 const PORT = process.env.PORT || 5000
 
 dotenv.config();
@@ -17,8 +17,10 @@ app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 
 mongoose
-
-    .connect("mongodb+srv://manohar:manohar@classroom.uwypa.mongodb.net/student?retryWrites=true&w=majority&appName=classroom")
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
